245e2b1991793938c9d5af3866bfe772fcd75f90
Gpocas <gpocas01@gmail.com>
2025-01-16 13:45:23 -0300

    :tada: initial commit

---
+++ dotfiles/config/fish/config.fish
@@ -0,0 +1,152 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -g -x fish_greeting ''
+end
+
+starship init fish | source
+
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+zoxide init fish | source
+echo 'mise activate fish | source' >>~/.config/fish/config.fish
+
+function storePathForWindowsTerminal --on-variable PWD
+    if test -n "$WT_SESSION"
+        printf "\e]9;9;%s\e\\" (wslpath -w "$PWD")
+    end
+end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# homebrew
+set --export HOMEBREW_NO_AUTO_UPDATE 1
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
+mise activate fish | source
