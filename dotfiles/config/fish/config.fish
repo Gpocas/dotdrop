9cbe2cd1b3146a3e83095ff55548ef3725f643de
Gpocas <gpocas01@gmail.com>
2025-04-01 16:05:03 -0300

    replace mise for asdf

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -7,6 +7,20 @@ end
 envload ~/.env.secrets
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
+# asdf
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
@@ -18,7 +32,6 @@ set --export HOMEBREW_NO_AUTO_UPDATE 1
 source "$HOME/.cargo/env.fish"
 
 zoxide init fish | source # initialize zoxide alias 'z'
-mise activate fish | source # initialize mise-en-place alias 'mise'
 starship init fish | source # initialize starship
 
 # alias
